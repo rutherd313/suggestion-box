7a01b6f8727bd993fb6c0014a9ee2b9dc61562af
John M. Dueno <djohnruther@yahoo.com>
2017-09-17 18:11:00 -0500

    first commit

---
+++ suggestion_box/js/controllers/HomeController.js
@@ -0,0 +1,20 @@
+app.controller("HomeController", ["$scope", "suggestions", function($scope, suggestions){
+	$scope.posts = suggestions.posts;
+
+	$scope.addSuggestion = function(){
+		if(!$scope.title || $scope.title === ""){
+			return;
+		}
+	$scope.posts.push({
+		title: $scope.title,
+		upvotes: 0,
+		comments: [],
+	});
+	$scope.title = "";
+	};
+
+	$scope.upVote = function(post){
+		post.upvotes += 1;
+	};
+
+}]);
---
+++ suggestion_box/js/controllers/SuggestionController.js
@@ -0,0 +1,29 @@
+app.controller("SuggestionController", ["$scope", "$routeParams", "suggestions", function($scope, $routeParams, suggestions){
+ $scope.post = suggestions.posts[$routeParams.id];
+
+ $scope.addComment = function(){
+ 	if(!$scope.comment || $scope.comment === ""){
+ 		return;
+ 	}
+ 	/*
+ 	$scope.comments.push({
+ 		body: $scope.comment,
+ 		upvotes: 0
+ 	});
+ 	*/
+ 	$scope.post.comments.push({
+	body: $scope.new_comment,  
+	upvotes: 0  	
+	}); 
+ 	
+ 	$scope.comment = "";
+ 	};
+
+ $scope.upVote = function(comment){
+ 	comment.upvotes += 1;
+ };
+ 
+}]);
+
+
+
